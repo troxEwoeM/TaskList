c505bddcaea6d88814206d8d9502e4f6a943a047
MeowExort <agit0.o@yandex.ru>
2017-12-10 14:42:02 +0300

    Cleanup

--- TaskList/TaskList/App.xaml.cs
+++ TaskList/TaskList/App.xaml.cs
@@ -1,15 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace TaskList {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application {
+﻿namespace TaskList {
+    public partial class App
+    {
     }
 }
